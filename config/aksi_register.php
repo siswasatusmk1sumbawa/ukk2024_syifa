a8b5a9ae138a08354c88953367fe529ac821c418
siswasatu <123527193+awaa29@users.noreply.github.com>
2024-02-01 08:02:22 +0800

    revisi-1
    penambahan aset. admin, dll

---
+++ config/aksi_register.php
@@ -0,0 +1,17 @@
+<?php 
+$username = $_POST['username'];
+$password = md5($_POST['password']);
+$email = $_POST['email'];
+$namalengkap = $_POST['namalengkap'];
+$alamat = $_POST['alamat'];
+
+$sql = mysqli_query($koneksi, "INSERT INTO user VALUES('','$username','$password','$email','$namalengkap','$alamat')");
+
+if ($sql) {
+	echo "<script>
+	aler('Pendaftaran akun berhasil');
+	location.href='../login.php';
+	</script>";
+}
+
+ ?>
