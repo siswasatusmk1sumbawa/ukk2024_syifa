71747dcbd4dfa08bf573f53eeec872c252183d19
siswasatu <siswasatu.smk1sumbawa@gmail.com>
2024-02-03 17:26:31 +0800

    revisi-4
    tambah aksi.album dan aksi.foto

---
+++ config/aksi_foto.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+include 'konkeis.php';
+
+if (isset($_POST['tambah'])) {
+	$judulfoto = $_POST['judulfoto'];
+	$deskripsifoto = $_POST['deskripsifoto'];
+	$tanggalunggah = date['Y-m-d'];
+    $albumid = $_POST['albumid'];
+	$userid = $_SESSION['userid'];
+    $foto = $_FILES['userid'];
+    $tmp = $_FILES['lokasifile'] ['tmp_name'];
+    $lokasi = '../aset/img/';
+    $namafoto = rand(). '-'.$foto;
+
+    move_uploaded_file($temp, $lokasi.$namafoto);
+
+	$sql = mysqli_query($koneksi,"INSERT INTO foto VALUES ('','$judulfoto','$deskripsifoto','$tanggalunggah','$albumid','$userid')");
+
+	echo "<script>
+	alert('Data berhasil disimpan!');
+	location.href='../admin/foto.php';
+	</script>";
+}
