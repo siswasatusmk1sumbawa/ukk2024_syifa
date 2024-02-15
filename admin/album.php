7ce1d50426c77f2316b9c3e613a990c442fdc4c8
siswasatu <siswasatu.smk1sumbawa@gmail.com>
2024-02-15 08:36:15 +0800

    fix
    fix

--- admin/album.php
+++ admin/album.php
@@ -9,7 +9,7 @@ if ($_SESSION['status'] != 'login') {
 }
 
 
- ?>
+?>
 
 <!DOCTYPE html>
 <html>
@@ -21,20 +21,19 @@ if ($_SESSION['status'] != 'login') {
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container">
+<div class="container">
     <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+	<span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
-      <div class="navbar-nav me-auto">
+    <div class="navbar-nav me-auto">
         <a href="album.php" class="nav-link">Albums</a>
 		<a href="foto.php" class="nav-link">Photos</a>
-      </div>
-   
-      <a href="../config/aksi_logout.php" class="btn btn-outline danger m-1">Logout</a>
     </div>
-  </div>
+    <a href="../config/aksi_logout.php" class="btn btn-outline danger m-1">Logout</a>
+    </div>
+</div>
 </nav>
 
 <div class="container mt-3">
@@ -62,8 +61,8 @@ if ($_SESSION['status'] != 'login') {
 							<input type="text" name="namaalbum" class="form-control" required>
 							<label class="form-label">Deskripsi</label>
 							<textarea class="form_control"
-							 name="deskripsi" required></textarea>
-							 <button type="submit" class="btn btn-primary mt-2" name="tambah">Tambah Data</button>
+							name="deskripsi" required></textarea>
+							<button type="submit" class="btn btn-primary mt-2" name="tambah">Tambah Data</button>
 						</form>
 					</div>
 				</div>
@@ -90,7 +89,7 @@ if ($_SESSION['status'] != 'login') {
 							$sql = mysqli_query($koneksi, "SELECT * FROM album WHERE userid='$userid'");
 							while ($data = mysqli_fetch_array($sql)) {
 							}
-							 ?>
+							?>
 							<tr>
 								<td><?php echo $no++ ?></td>
 								<td><?php echo $data['namaalbum'] ?></td>
@@ -98,23 +97,23 @@ if ($_SESSION['status'] != 'login') {
 								<td><?php echo $data['tanggalbuat'] ?></td>
 								<td>
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit <?php echo $data['albumid']; ?>">
-  Edit
+Edit
 </button>
-<div class="modal fade" id="edit <?php echo $data['albumid']; ?>">" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+<div class="modal fade" id="edit <?php echo $data['albumid'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header">
+	<div class="modal-header">
         <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Data</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
+    </div>
+    <div class="modal-body">
         <form action="../config/aksi_album.php" method="POST">
         	<input type="hidden" name="albumid" value="<?php echo $data['albumid'] ?>">
         	<label class="form-label">Nama Album</label>
 							<input type="text" name="namaalbum" value="<?php echo $data ['namaalbum'] ?>" class="form-control" required>
 							<label class="form-label">Deskripsi</label>
 							<textarea class="form_control"
-							 name="deskripsi" required><?php echo $data ['deskripsi'] ?></textarea>
+							name="deskripsi" required><?php echo $data ['deskripsi'] ?></textarea>
       </div>
       <div class="modal-footer">
         <button type="submit" name="edit" class="btn btn-primary">Edit Data</button>
